f752e0493ddaf9f8bee9bbf10f6f49534cd9283a
Lyubomir Koev <lyubomir.koev@atia.com>
2014-06-24 16:12:18 +0300

    Fix beams attaching and limit the angles

--- project/cissoid.cpp
+++ project/cissoid.cpp
@@ -6,7 +6,7 @@ double vlen(vect && v)
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
 
-void attach_beams(Beam & left, Beam & right)
+void attach_beams(Beam & left, Beam & right, bool use_pos = true)
 {
     double r1 = left.getSize(), r2 = right.getSize(),
            dist = vlen(left.getCenter() - right.getCenter());
@@ -29,13 +29,19 @@ void attach_beams(Beam & left, Beam & right)
     vect pt1 = { (cx2 + h * (right.getCenter().y - left.getCenter().y) / dist), (cy2 - h * (right.getCenter().x - left.getCenter().x) / dist), 0, 0};
     vect pt2 = { (cx2 - h * (right.getCenter().y - left.getCenter().y) / dist), (cy2 + h * (right.getCenter().x - left.getCenter().x) / dist), 0, 0};
 
-    left.setH(atan2(pt1.y - left.getCenter().y, pt1.x - left.getCenter().x) * 180 / M_PI);
-    right.setH(atan2(pt2.y - right.getCenter().y, pt2.x - right.getCenter().x) * 180 / M_PI);
+    if(use_pos) {
+        left.setH(atan2(pt1.y - left.getCenter().y, pt1.x - left.getCenter().x) * 180 / M_PI);
+        right.setH(atan2(pt1.y - right.getCenter().y, pt1.x - right.getCenter().x) * 180 / M_PI);
+    } else {
+        left.setH(atan2(pt2.y - left.getCenter().y, pt2.x - left.getCenter().x) * 180 / M_PI);
+        right.setH(atan2(pt2.y - right.getCenter().y, pt2.x - right.getCenter().x) * 180 / M_PI);
+    }
+
 }
 
 class cissoid_cls: public MechoDev {
-    const int mult1 = 2, mult2 = 3;
-    double size, radius, offset, dir;
+    const int mult1 = 2.9, mult2 = 3.5;
+    double size, radius, offset, dir, stop_angle;
     vect center;
     Pencil pen;
 
@@ -47,62 +53,75 @@ class cissoid_cls: public MechoDev {
 public:
     cissoid_cls(double, const vect &);
     void setTime(double);
-
+private:
+    void fixHeights();
 };
 
 cissoid_cls::cissoid_cls(double size, const vect & center):
-    offset(0), dir(1),
+    offset(0), dir(1), stop_angle(55),
     size(size), pen(size), rotator(size), center(center),
     pivot_up(size * mult1), pivot_down(size * mult1),
     drawer_up(size * mult1), drawer_down(size * mult1),
     control_up(size * mult2), control_down(size * mult2)
 {
-
+    setSpeed(50);
     rotator.setCenter(center);
 
     control_up.setCenter(rotator.otherPoint());
     control_down.setCenter(rotator.otherPoint());
 
-    control_up.setH(90);
-    control_down.setH(90);
-
     pivot_up.setCenter({center.x + size, center.y, center.z, center.w});
     pivot_down.setCenter({center.x + size, center.y, center.z, center.w});
 
-    pivot_down.setZ(2);
-    pivot_up.setZ(2);
-
-    pivot_up.setH(90);
-    pivot_down.setH(90);
-
     drawer_up.setCenter(control_up.otherPoint());
     drawer_down.setCenter(control_down.otherPoint());
+
+    offset = stop_angle;
+    setTime(0);
+
+    pen.penDown();
+    fixHeights();
 }
 
 
 void cissoid_cls::setTime(double time)
 {
-    if(dir == 1 && offset >= 360) {
+    if(dir == 1 && offset >= 360 - stop_angle) {
         dir = -1;
-    } else if(dir == -1 && offset <= 0) {
+    } else if(dir == -1 && offset <= stop_angle) {
         dir = 1;
     }
 
-    offset += dt * 100 * dir;
+    offset += dt * getSpeed() * dir;
     rotator.setH(offset);
 
     control_up.setCenter(rotator.otherPoint());
     control_down.setCenter(rotator.otherPoint());
 
-    attach_beams(control_up, pivot_up);
-    attach_beams(control_down, pivot_down);
+    attach_beams(control_up, pivot_up, false);
+    attach_beams(control_down, pivot_down, true);
 
     drawer_up.setCenter(pivot_up.otherPoint());
     drawer_down.setCenter(pivot_down.otherPoint());
 
-    control_down.setZ(2);
-    control_up.setZ(3);
+    attach_beams(drawer_down, drawer_up, true);
 
+    fixHeights();
+    pen.setCenter(drawer_up.otherPoint().setZ(0));
+}
+
+void cissoid_cls::fixHeights()
+{
+    rotator.setZ(3);
+
+    control_down.setZ(2);
+    control_up.setZ(2);
+
+    pivot_down.setZ(1);
+    pivot_up.setZ(1);
+
+    drawer_down.setZ(0);
+    drawer_up.setZ(0);
 }
 
 int main()
